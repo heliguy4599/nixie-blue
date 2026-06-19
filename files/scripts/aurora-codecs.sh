9e8fb23ad41ccd5da3a77927a84ac557fcd55538
heliguy4599 <heliguy4599@hotmail.com>
2026-06-18 21:05:09 -0400

    Attempt to fix codec issue

--- files/scripts/aurora-codecs.sh
+++ files/scripts/aurora-codecs.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-dnf -y distro-sync \
-	ffmpeg \
-	libheif \
-	libva \
+dnf5 config-manager addrepo \
+	--from-repofile="https://negativo17.org/repos/fedora-multimedia.repo"
+
+dnf5 config-manager setopt fedora-multimedia.priority=90
+
+dnf5 -y distro-sync \
+	--repo=fedora-multimedia \
 	mesa-filesystem \
 	mesa-dri-drivers \
 	mesa-va-drivers \
 	mesa-vulkan-drivers \
+	libva \
+	libheif
+
+dnf5 -y install \
+	--repo=fedora-multimedia \
+	ffmpeg \
 	pipewire-libs-extra \
 	x264-libs \
 	x265-libs
