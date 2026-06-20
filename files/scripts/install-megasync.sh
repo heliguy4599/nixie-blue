3f00689ec8f2247fdb00ccc024d2f6ffc72ded97
Heliguy <95997226+heliguy4599@users.noreply.github.com>
2026-06-19 23:33:53 -0400

    Fix install-megasync.sh for better execution
    Update install script to use relative path and add -y flag.

--- files/scripts/install-megasync.sh
+++ files/scripts/install-megasync.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-wget https://mega.nz/linux/repo/Fedora_44/x86_64/megasync-Fedora_44.x86_64.rpm && dnf install "$PWD/megasync-Fedora_44.x86_64.rpm"
+wget https://mega.nz/linux/repo/Fedora_44/x86_64/megasync-Fedora_44.x86_64.rpm
+dnf install -y "./megasync-Fedora_44.x86_64.rpm"
