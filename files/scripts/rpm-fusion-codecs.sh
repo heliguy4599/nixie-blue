9feacd2dea7bc1cfaebcf9cfce968ebc36656d05
heliguy4599 <heliguy4599@hotmail.com>
2026-07-14 00:02:49 -0400

    Attempt four

--- files/scripts/rpm-fusion-codecs.sh
+++ files/scripts/rpm-fusion-codecs.sh
@@ -3,5 +3,5 @@ set -oue pipefail
 
 dnf swap -y ffmpeg-free ffmpeg --allowerasing
 dnf install -y @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-dnf install mesa-va-drivers-freeworld
-dnf install mesa-va-drivers-freeworld.i686
+dnf install -y mesa-va-drivers-freeworld
+dnf install -y mesa-va-drivers-freeworld.i686
